271ab223e66656d488dc0c7255cf8117f8c23880
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-06 20:28:38 +0300

    task 0 update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator():
     Then yield a random number between 0 and 10'''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.random() * random.randint(0, 10)
